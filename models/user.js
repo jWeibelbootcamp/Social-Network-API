5ed8f4c585e7aa617fa650443fdddfd75d899101
devWeibel <weibel.jason@gmail.com>
2022-11-12 10:59:24 -0500

    user model skeleton

--- models/user.js
+++ models/user.js
@@ -1,2 +1,20 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');  // does this need Types?
 
+const userSchema = new Schema({  // does this not need to be new mongoose.Schema?
+    username: {
+
+    },
+    email: {
+
+    },
+    thoughts: {
+
+    },
+    friends: {
+
+    }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User; 
