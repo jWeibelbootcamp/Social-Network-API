1945e8d6dbe35696a493d0fd43c0108d23753de0
devWeibel <weibel.jason@gmail.com>
2022-11-12 08:25:40 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./config/config.js');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT ${PORT}.`);
+    });
+});
