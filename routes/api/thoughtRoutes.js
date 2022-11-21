8aede28dd347483813ad5ade18e8d49e64486e2f
devWeibel <weibel.jason@gmail.com>
2022-11-20 20:49:55 -0500

    working on thought api bugs

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -16,10 +16,10 @@ router.route('/').get(getAllThoughts).post(createThought);
 // localhost:3001/api/thoughts/:thoughtId
 router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
 
-// localhost:3001/api/thoughts/:thoughtId/reaction
+// localhost:3001/api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions').post(addReaction);
 
-// localhost:3001/api/thoughts/:thoughtId/reaction/:reactionId
+// localhost:3001/api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
